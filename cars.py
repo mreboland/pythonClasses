6e26f11804274287c19c442a70413a16e1206617
Edward Boland <mr.eboland@gmail.com>
2021-01-22 20:35:07 -0500

    init commit, and expanding module to facilitate lesson

--- cars.py
+++ cars.py
@@ -38,3 +38,15 @@ class Car:
             self.odometerReading += miles
         else:
             print("You can't have negative miles...")
+
+# Adding the below for myElectricCar.py (storing multiple classes)
+class ElectricCar(Car):
+
+    """Models aspects of a car, specific to electric vehicles."""
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
