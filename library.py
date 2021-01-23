47601a5e42f8f1b2f4ad55bf0d8b299e8a8cc837
Edward Boland <mr.eboland@gmail.com>
2021-01-22 22:07:22 -0500

    styling classes

--- library.py
+++ library.py
@@ -98,3 +98,23 @@ while True:
         print(f"It took {draws} draws to win!")
         break
 
+# Styling classes
+# Class names should be written in CamelCase.
+
+# Instance and module names should be written in lowercase with underscores between words.
+
+# Every class should have a docstring immediately following the class definition.
+# The docstring should be a brief description of what the class does,
+# and you should follow the same formatting conventions you used for writing
+# docstrings in functions. Each module should also have a docstring describing
+# what the classes in a module can be used for.
+
+# You can use blank lines to organize code, but don’t use them excessively.
+# Within a class you can use one blank line between methods, and
+# within a module you can use two blank lines to separate classes.
+
+# If you need to import a module from the standard library and a module
+# that you wrote, place the import statement for the standard library module
+# first. Then add a blank line and the import statement for the module you
+# wrote. In programs with multiple import statements, this convention makes
+# it easier to see where the different modules used in the program come from.
